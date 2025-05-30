79589daf583f3abf56dc8721fe5ce7fa83c495db
Meenakshi Kumari <meenakshigauri17@gmail.com>
2025-05-30 12:21:55 +0530

    Add files via upload

---
+++ find_eventual_safe_states.java
@@ -0,0 +1,39 @@
+class Solution {
+    //topo sort algo(one brute algo using cycle detection in directed graphs)
+    public boolean dfsCheck(int node, int[][] graph, int[] vis, int[] pathVis){
+        vis[node] = 1;
+        pathVis[node] = 1;
+
+        for(int it : graph[node]){
+            if(vis[it] == 0){
+                if(dfsCheck(it,graph, vis, pathVis)){
+                    return true;
+                }
+            }
+            else if(pathVis[it] == 1){
+                return true;
+            }
+        }
+
+        pathVis[node] = 0;
+        return false;
+    }
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        int V = graph.length;
+        int[] vis = new int[V];
+        int[] pathVis = new int[V];
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for(int i=0; i<V; i++){
+            if(vis[i] == 0){
+                dfsCheck(i,graph,vis,pathVis);
+            }
+        }
+
+        for(int i=0; i<V; i++){
+            if(pathVis[i] == 0)
+            ans.add(i);
+        }
+        return ans;
+    }
+}
