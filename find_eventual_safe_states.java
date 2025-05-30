6d3cd2a6efc1b3c5faa39b109098fadc1704f9da
Meenakshi Kumari <meenakshigauri17@gmail.com>
2025-05-30 19:13:01 +0530

    Update find_eventual_safe_states.java

--- find_eventual_safe_states.java
+++ find_eventual_safe_states.java
@@ -36,4 +36,47 @@ class Solution {
         }
         return ans;
     }
+
+*************************************************** TOPOSORT SOLUTION *****************************************************************
+    //topo sort
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        ArrayList<ArrayList<Integer>> adjRev = new ArrayList<>();
+        int V = graph.length;
+        int[] indegree = new int[V];
+
+        for(int i=0; i<V; i++){
+            adjRev.add(new ArrayList<>());
+        }
+
+        for(int i=0; i<V; i++){
+            for(int it : graph[i]){
+                adjRev.get(it).add(i);
+                indegree[i]++;
+            }
+        }
+
+        ArrayList<Integer> safenodes = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i=0; i<V; i++){
+            if(indegree[i] == 0){
+                q.add(i);
+            }
+        }
+
+        while(!q.isEmpty()){
+            int node = q.remove();
+            safenodes.add(node);
+
+            for(int it : adjRev.get(node)){
+                indegree[it]--;
+                if(indegree[it] == 0){
+                    q.add(it);
+                }
+            }
+        }
+
+        Collections.sort(safenodes);
+        return safenodes;
+    }
 }
